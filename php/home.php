337ed54dccb7eea3858d9c6c78b1e0623da545ad
yutasato1112 <58802670+yutasato1112@users.noreply.github.com>
2022-04-30 00:27:25 +0900

    add header and footer

--- php/home.php
+++ php/home.php
@@ -4,9 +4,9 @@
     <meta name="description" content="ECサイト">
     <link rel="icon" type="image/svg+xml" href="../picture/general/shopping_favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/css_home.css">
     <meta charset="utf-8">
     <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/css_home.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Luxurious+Roman&display=swap" rel="stylesheet">
@@ -14,20 +14,28 @@
 
 </header>
 <body>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <button type="button" onclick="location.href='home.php'" class="title">ECサイト</button>
+    <div class="log_and_reg_area">
+        <button type="button" onclick="location.href='login.php'" class="btn btn-primary btn-lg log">ログイン</button>
+        <button type="button" onclick="location.href='register.php'" class="btn btn-primary btn-lg reg">新規登録</button>
+    </div>
+    <div class="serch_area">
+        <div class="space"></div>
+        <form action="serch.php" method="GET" class="serch_keyword">
+            <input class="form-control form-control-lg keyword" type="text" placeholder="検索キーワード" aria-label=".form-control-lg" name="res_name">
+            <button type="submit" class="btn btn-secondary kensaku">検索</button>
+        </form>
+        <div class="space"></div>
+    </div>
 
-    <?php
-        $dsn = 'pgsql:dbname=dl7k5i97ich1l host=ec2-34-194-73-236.compute-1.amazonaws.com port=5432';
-        $user = 'vmarkahoqhzaaa';
-        $password = '432da7483948509568cbe6ee852bc3f3ae993e318323455efd363d5866623b17';
-        
-        try{
-            $dbh = new PDO($dsn, $user, $password);
-            print "ok";
-        }catch (PDOException $e){
-            print('Error:'.$e->getMessage());
-            die();
-        }
-    ?>
+    <div class="main_contents">
+        <!-- ここにメインコンテンツを記述 -->
+        <h1>メインコンテンツ(作成時にはこの行は消去してください)<h1>
 
+        <!-- メインコンテンツここまで -->
+    </div>
+    <div class="footer">
+        <p class="copy_right">©yutasato & yukioda</p>
+    </div>
 </body>
---
+++ php/serch.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+<header>
+    <meta name="description" content="ECサイト">
+    <link rel="icon" type="image/svg+xml" href="../picture/general/shopping_favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/css_serch.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Luxurious+Roman&display=swap" rel="stylesheet">
+    <title>検索結果</title>
+
+</header>
+<body>
+    <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+    <button type="button" onclick="location.href='home.php'" class="title">ECサイト</button>
+    <div class="log_and_reg_area">
+        <button type="button" onclick="location.href='login.php'" class="btn btn-primary btn-lg log">ログイン</button>
+        <button type="button" onclick="location.href='register.php'" class="btn btn-primary btn-lg reg">新規登録</button>
+    </div>
+    <div class="serch_area">
+        <div class="space"></div>
+        <form action="serch.php" method="GET" class="serch_keyword">
+            <input class="form-control form-control-lg keyword" type="text" placeholder="検索キーワード" aria-label=".form-control-lg" name="res_name">
+            <button type="submit" class="btn btn-secondary kensaku">検索</button>
+        </form>
+        <div class="space"></div>
+    </div>
+
+    <div class="main_contents">
+        <!-- ここにメインコンテンツを記述 -->
+        <h1>メインコンテンツ(作成時にはこの行は消去してください)<h1>
+
+        <!-- メインコンテンツここまで -->
+    </div>
+    <div class="footer">
+        <p class="copy_right">©yutasato & yukioda</p>
+    </div>
+</body>
